4465e11320f8336a29b44ac48cd566b7b1dfb0a0
Neoclassic VII <99411270+NeoclassicVII@users.noreply.github.com>
2022-03-22 19:46:59 +0100

    New Update [22.03.22, 19:45]

--- lastfm.py
+++ lastfm.py
@@ -1,57 +1,51 @@
 # LastFM Web Crawler for Metalheads by Neoclassic VII
 # Written according to the principle: "If it works - do not touch it!". I could have found a better way to write this code, but it works fine as it is.
-import webbrowser 
+import time
 import requests
-import difflib, time
+import webbrowser 
+import difflib
+import colorama
 from colorama import Fore
 from bs4 import BeautifulSoup
 
-def nl():
-	print("\n")
+colorama.init()
 
-def lastfm(name):
+genres = [
+"deathcore","power+metal","heavy+metal","death+metal","black+metal","groove+metal","thrash+metal","alternative+metal","nu+metal","folk+black+metal",
+"trve+kvlt","symphonic+black+metal","ambient+black+metal","technical+deathcore","blackened+technical+death+metal","norwegian+black+metal","brutal+death+metal",
+"neoclassical+death+metal","neoclassical+metal","electronicore","doom+metal","gothic+metal","djent","glam+metal","metalcore","groove+metal","progressive+metal",
+"prog+metal","avant+garde+metal","folk+metal","grindcore","melodic+black+metal","cyber+metal","melodic+death+metal","mathcore","white+metal","industrial+metal",
+"symphonic+deathcore","magical+death+metal","neoclassical+deathcore","pornogore","folk+deathcore","folk+death+metal"
+]
 
-	genres = ["deathcore","power+metal","heavy+metal","death+metal","black+metal","groove+metal","thrash+metal","alternative+metal","nu+metal","folk+black+metal"]
+def lastfm(genre):
+  url = "https://www.last.fm/tag/"+str(', '.join(difflib.get_close_matches(genre,genres,1)))+"/artists"
+  source = requests.get(url).text
+  soup = BeautifulSoup(source, "lxml")
 
-	source = requests.get("https://www.last.fm/tag/"+str(', '.join(difflib.get_close_matches(name,genres,1)))+"/artists").text
+  bandlist = soup.find("ol", class_="big-artist-list")
 
-	soup = BeautifulSoup(source, "lxml")
-	ol = soup.find("ol", class_="big-artist-list")
+  try:
+    if not bandlist.text == "None":
+      print()
+      time.sleep(0.5)
+      print(Fore.GREEN + "[+] Last FM Page Found!")
+      print(Fore.RED + "URL: " + Fore.BLUE + url)  
+      print()
 
-	try:
-		if not ol.text == "None":
-			nl()
-			time.sleep(0.5)
-			print(Fore.GREEN + "[+] Last FM Page Found!" + Fore.CYAN)
-			nl()  
-          	
+      bandname = bandlist.find_all("h3", class_="big-artist-list-title")
+      listeners = bandlist.find_all("p", class_="big-artist-list-listeners")
+      
+      print(Fore.YELLOW + "Bands:")
+      print()
 
-			ilone =	ol.find("li")
-			time.sleep(0.5)
-			firstband = print(ilone.h3.text)
-			firstlisten = print(ilone.p.text.strip())
-			firstinfo = ilone.find("div", class_="big-artist-list-bio")
-			nl()
-			print(firstinfo.text)
+      for i in range(len(bandname)):
+        print(Fore.RED + bandname[i].text + " " + Fore.WHITE + listeners[i].text.strip())
 
-			nl()
-			time.sleep(0.5)
-			print(Fore.YELLOW + "_"*139 +  Fore.CYAN)
-			nl()
+    else:
+      pass
 
-			ilonenext = ilone.find_next("li")
-			ilonenext2 = ilonenext.find_next("li")
-			firstband2 = print(ilonenext2.h3.text)
-			firstlisten2 = print(ilonenext2.p.text.strip())
-			firstinfo2 = ilonenext2.find("div", class_="big-artist-list-bio").p
-			nl()
-			print(firstinfo2.text)
-			time.sleep(0.5)
-
-		else:
-			pass
-
-	except AttributeError:
-		time.sleep(0.5)
-		print("[-] No Last FM page found ")
-		pass
+  except AttributeError:
+    time.sleep(0.5)
+    print("[-] No Last FM page found ")
+    pass
