8e1429b1e6f514a8bcf052a09e907d5f113aebe8
Neoclassic VII <99411270+NeoclassicVII@users.noreply.github.com>
2022-03-26 18:16:36 +0100

    New Update [26.03.22, 18:16]
    - New Design
    - More News
    - Fixed Errors/Bugs
    etc...

--- metalinjection.py
+++ metalinjection.py
@@ -1,4 +1,6 @@
-# Daily Metal News by Neoclassic VII!
+# Daily Metal News by Neoclassic VII
+# from [metalincetion.com]
+
 import time
 import requests
 import webbrowser
@@ -11,17 +13,25 @@ colorama.init()
 URL = "https://metalinjection.net/"
 
 def metalnews():
-	try:
-		source = requests.get(URL).text
-		soup = BeautifulSoup(source, "lxml")
+  try:
+    source = requests.get(URL).text
+    soup = BeautifulSoup(source, "lxml")
 
-		findnews = soup.find("div", class_="zox-art-text")
-		print(Fore.RED + findnews.div.h3.text)
-		titel = findnews.find("div", class_="zox-art-title")
-		print(Fore.GREEN + titel.text)
-		moreinfo = findnews.find("p", class_="zox-s-graph")
-		print(Fore.GREEN + moreinfo.text)
-		print(Fore.WHITE)
+    findnews = soup.find("div", class_="zox-feat-ent1-cont zoxrel")
+    titel = findnews.find_all("div", class_="zox-art-title")
+    print(Fore.LIGHTBLACK_EX + "#" * 58)
+    print(Fore.WHITE)
+    print(Fore.RED + "[Latest News]".center(55) + Fore.WHITE)
+    moreinfo = findnews.find_all("p", class_="zox-s-graph")
 
-	except ConnectionError:
-		pass
+    for i in range(len(titel)):
+        print(Fore.YELLOW + "  " + titel[i].text + " " + Fore.LIGHTCYAN_EX + moreinfo[i].text + Fore.WHITE)
+    print()
+    print(Fore.LIGHTBLACK_EX + "#" * 58)
+    print(Fore.WHITE)
+
+  except (requests.ConnectionError, requests.Timeout) as exception:
+  	print(Fore.RED + "[Latest News]".center(55) + Fore.WHITE)
+  	print(Fore.RED + "[-] No Internet Connection!".center(55))
+  	print(Fore.WHITE)
+  	pass
