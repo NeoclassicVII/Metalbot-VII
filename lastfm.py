ad7f2e588d1b1ba88dcab33fbcccdef2c4836986
Neoclassic VII <99411270+NeoclassicVII@users.noreply.github.com>
2022-03-27 16:01:18 +0200

    New Update [27.03.22, 16:00]
    New genres added, errors fixed

--- lastfm.py
+++ lastfm.py
@@ -11,12 +11,13 @@ from bs4 import BeautifulSoup
 colorama.init()
 
 genres = [
-"deathcore","power+metal","heavy+metal","death+metal","black+metal","groove+metal","thrash+metal","alternative+metal","nu+metal","folk+black+metal",
+"deathcore","power+metal","heavy+metal","death+metal","black+metal","thrash+metal","alternative+metal","nu+metal","folk+black+metal",
 "trve+kvlt","symphonic+black+metal","ambient+black+metal","technical+deathcore","blackened+technical+death+metal","norwegian+black+metal","brutal+death+metal",
 "neoclassical+death+metal","neoclassical+metal","electronicore","doom+metal","gothic+metal","djent","glam+metal","metalcore","groove+metal","progressive+metal",
 "prog+metal","avant+garde+metal","folk+metal","grindcore","melodic+black+metal","cyber+metal","melodic+death+metal","mathcore","white+metal","industrial+metal",
-"symphonic+deathcore","melodic+deathcore","slamming+deathcore","beatdown+deathcore","slamming+beatdown+deathcore","magical+death+metal","neoclassical+deathcore","pornogore",
-"folk+deathcore","folk+death+metal","slamming+brutal+death+metal"
+"symphonic+deathcore","melodic+deathcore","slamming+deathcore","beatdown+deathcore","slamming+beatdown+deathcore","magical+death+metal",
+"neoclassical+deathcore","pornogore","folk+deathcore","folk+death+metal","slamming+brutal+death+metal","powerviolence","crossover","shitgrind",
+"pirate+metal","kawaii+metal","drone+metal","drone","math+metal","neue+deutsche+härte","ndh","technical+death+metal","black+death+metal"
 ]
 
 def lastfm(genre):
